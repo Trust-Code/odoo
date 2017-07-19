920b6b8e055903c388cd6b66ef1abc1502615a87
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-07-19 14:38:24 -0300

    FIX - Mass mail was just adding a note to partner Change type to email and fixed the list of partners to add as followers

--- addons/mail/res_partner.py
+++ addons/mail/res_partner.py
@@ -64,7 +64,7 @@ class res_partner_mail(osv.Model):
         if kwargs.get('type') == 'email':
             partner_ids = kwargs.get('partner_ids', [])
             if thread_id not in [command[1] for command in partner_ids]:
-                partner_ids.append((4, thread_id))
+                partner_ids.append(thread_id)
             kwargs['partner_ids'] = partner_ids
             thread_id = False
         return super(res_partner_mail, self).message_post(cr, uid, thread_id, **kwargs)
